5a3a5a3f119386a47484e607db099b11aeaec855
luizMM <luizmmb1@al.insper.edu>
2025-02-16 15:52:31 -0300

    exercios 1 a 2 feitos

--- exe3/main.c
+++ exe3/main.c
@@ -2,9 +2,39 @@
 #include "pico/stdlib.h"
 #include <stdio.h>
 
+const int PIN_R_IN = 28;
+const int PIN_G_IN = 26;
+const int PIN_R_OUT = 4;
+const int PIN_G_OUT = 6;
+const bool r_state = false;
+const bool g_state = false;
+
+
+
 int main() {
   stdio_init_all();
 
+  gpio_init(PIN_R_IN);
+  gpio_init(PIN_G_IN);
+  gpio_init(PIN_R_OUT);
+  gpio_init(PIN_G_OUT);
+
+  gpio_set_dir(PIN_R_IN, GPIO_IN);
+  gpio_set_dir(PIN_G_IN, GPIO_IN);
+  gpio_set_dir(PIN_R_OUT, GPIO_OUT);
+  gpio_set_dir(PIN_G_OUT, GPIO_OUT);
+
+  gpio_pull_up(PIN_R_IN);
+  gpio_pull_up(PIN_G_IN);
+  
   while (true) {
+    if (!gpio_get(PIN_R_IN)){
+      gpio_put(PIN_R_OUT, 1);
+      
+    }
+
+    if (!gpio_get(PIN_G_IN)){
+      gpio_put(PIN_G_OUT, 1);
+    }
   }
 }
