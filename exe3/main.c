af6faca909321968356e3f16b96d1290cd3c76a8
luizMM <luizmmb1@al.insper.edu.br>
2025-02-16 19:03:04 -0300

    questao 4

--- exe3/main.c
+++ exe3/main.c
@@ -6,9 +6,10 @@ const int PIN_R_IN = 28;
 const int PIN_G_IN = 26;
 const int PIN_R_OUT = 4;
 const int PIN_G_OUT = 6;
-const bool r_state = false;
-const bool g_state = false;
-
+bool r_state = false;
+bool g_state = false;
+bool r_button_last_state = true;
+bool g_button_last_state = true;
 
 
 int main() {
@@ -28,13 +29,20 @@ int main() {
   gpio_pull_up(PIN_G_IN);
   
   while (true) {
-    if (!gpio_get(PIN_R_IN)){
-      gpio_put(PIN_R_OUT, 1);
-      
-    }
+    bool r_button_current_state = gpio_get(PIN_R_IN);
 
-    if (!gpio_get(PIN_G_IN)){
-      gpio_put(PIN_G_OUT, 1);
+    if (r_button_current_state == 0 && r_button_last_state == 1) {
+        r_state = !r_state;
+        gpio_put(PIN_R_OUT, r_state);
     }
+    r_button_last_state = r_button_current_state;
+
+    bool g_button_current_state = gpio_get(PIN_G_IN);
+    
+    if (g_button_current_state == 0 && g_button_last_state == 1) {
+      g_state = !g_state;
+        gpio_put(PIN_G_OUT, g_state);
+    }
+    g_button_last_state = g_button_current_state;
   }
 }
--- exe4/main.c
+++ exe4/main.c
@@ -2,10 +2,63 @@
 #include "pico/stdlib.h"
 #include <stdio.h>
 
+const int pin_botao = 28;
+const int pin_r = 5;
+const int pin_p = 8;
+const int pin_b = 11;
+const int pin_y = 15;
+
 int main() {
   stdio_init_all();
 
+  gpio_init(pin_botao);
+  gpio_init(pin_r);
+  gpio_init(pin_p);
+  gpio_init(pin_b);
+  gpio_init(pin_y);
+
+  gpio_set_dir(pin_botao, GPIO_IN);
+  gpio_set_dir(pin_r, GPIO_OUT);
+  gpio_set_dir(pin_p, GPIO_OUT);
+  gpio_set_dir(pin_b, GPIO_OUT);
+  gpio_set_dir(pin_y, GPIO_OUT);
+
+  gpio_pull_up(pin_botao);
+
+  bool botao_last_state = false;
+
+
   while (true) {
     // Use delay de 300 ms entre os estados!
+    bool botao_state = gpio_get(pin_botao);
+    
+    if (botao_state == 0 && botao_last_state == 1){
+      gpio_put(pin_r, 1);
+      gpio_put(pin_p, 0);
+      gpio_put(pin_b, 0);
+      gpio_put(pin_y, 0);
+      sleep_ms(300);
+      gpio_put(pin_r, 0);
+      gpio_put(pin_p, 1);
+      gpio_put(pin_b, 0);
+      gpio_put(pin_y, 0);
+      sleep_ms(300);
+      gpio_put(pin_r, 0);
+      gpio_put(pin_p, 0);
+      gpio_put(pin_b, 1);
+      gpio_put(pin_y, 0);
+      sleep_ms(300);
+      gpio_put(pin_r, 0);
+      gpio_put(pin_p, 0);
+      gpio_put(pin_b, 0);
+      gpio_put(pin_y, 1);
+      sleep_ms(300);
+      gpio_put(pin_r, 0);
+      gpio_put(pin_p, 0);
+      gpio_put(pin_b, 0);
+      gpio_put(pin_y, 0);
+    }
+
+    botao_last_state = botao_state;
   }
 }
